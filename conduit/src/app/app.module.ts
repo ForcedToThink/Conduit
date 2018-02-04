5c7eb6ce0d01ce29076fa8ace718c01919afe0ea
Dawid Lipski <dawidlipski89@gmail.com>
2018-02-04 01:33:48 +0100

    basic layout and routing

--- conduit/src/app/app.module.ts
+++ conduit/src/app/app.module.ts
@@ -1,16 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule, ModuleWithProviders } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
+import { SharedModule, HeaderComponent, FooterComponent } from './shared';
 
 import { AppComponent } from './app.component';
+import { HomeModule } from './home/home.module';
 
+const rootRouting: ModuleWithProviders = RouterModule.forRoot([], { useHash: true });
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    SharedModule,
+    HomeModule,
+    rootRouting
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ conduit/src/app/home/home.module.ts
@@ -0,0 +1,19 @@
+import { NgModule, ModuleWithProviders } from '@angular/core';
+import { RouterModule } from '@angular/router';
+
+import { HomeComponent } from './home.component';
+import { SharedModule } from '../shared';
+
+const homeRouting: ModuleWithProviders = RouterModule.forChild([
+  {
+    path: '',
+    component: HomeComponent
+  }
+]);
+
+@NgModule({
+  declarations: [ HomeComponent ],
+  imports: [ homeRouting, SharedModule],
+  providers: [],
+})
+export class HomeModule {}
